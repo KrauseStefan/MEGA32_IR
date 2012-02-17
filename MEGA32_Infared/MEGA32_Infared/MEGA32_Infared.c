056a0d379e44fc17ca21d66bcd609a9ae84a94dd
Vinnie Juul <Trolde_Puk@hotmail.com>
2012-02-17 13:30:37 +0100

    Empty project

---
+++ MEGA32_Infared/MEGA32_Infared/MEGA32_Infared.c
@@ -0,0 +1,16 @@
+/*
+ * MEGA32_Infared.c
+ *
+ * Created: 17-02-2012 13:18:03
+ *  Author: Vinnie Juul
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
